3a8906024aea110548dc38ade4b27f9cec6ecd5f
ilahamusayeva93 <ilahamusayeva06@gmail.com>
2024-09-09 10:35:39 -0400

    Implemented spacer extraction functionality

--- extract_spacers.sh
+++ extract_spacers.sh
@@ -1,45 +1,39 @@
 #!/bin/bash
 
-# Check for the correct number of arguments
+
 if [ "$#" -ne 3 ]; then
     echo "Usage: $0 <query file> <subject file> <output file>"
     exit 1
 fi
 
-query_file=$1  # CRISPR repeat sequence (query)
-subject_file=$2  # Genome assembly (subject)
-output_file=$3  # Output file for extracted spacers
+query_file=$1
+subject_file=$2
+output_file=$3
 
-# Get the length of the query sequence
 query_length=$(grep -v ">" "$query_file" | tr -d '\n' | wc -c)
-
-# Perform BLAST to find perfect matches (CRISPR repeats)
 blastn -query "$query_file" -subject "$subject_file" -task blastn-short \
 -outfmt '6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore' > temp_output.txt
 
-# Identify CRISPR repeat locations and prepare the BED file for spacers
-awk -v len="$query_length" 'BEGIN { prev_end = 0 }
-{
-    # Identify perfect matches and print spacer coordinates to BED file
-    if ($3 == 100 && $4 == len && $5 == 0 && $6 == 0) {
-        if (prev_end > 0) {
-            spacer_start = prev_end + 1;
-            spacer_end = $7 - 1;
-            if (spacer_start < spacer_end) {
-                print $2 "\t" spacer_start "\t" spacer_end;
-            }
+awk -v len="$query_length" '$3 == 100 && $4 == len && $5 == 0 && $6 == 0' temp_output.txt > perfect_matches.bed
+
+awk '{
+    if (NR > 1) {
+        spacer_start = prev_end_pos + 1;
+        spacer_end = $9 - 1;
+        if (spacer_end > spacer_start) {
+            print $2 "\t" spacer_start "\t" spacer_end;
         }
-        prev_end = $8;
     }
-}' temp_output.txt > spacers.bed
+    prev_end_pos = $10;
+}' perfect_matches.bed > spacer_regions.bed
 
-# Use seqtk to extract the spacer sequences from the genome assembly
-seqtk subseq "$subject_file" spacers.bed > "$output_file"
 
-# Count and print the number of spacers extracted
-spacer_count=$(grep -c ">" "$output_file")
-echo "Number of spacers extracted: $spacer_count"
+seqtk subseq "$subject_file" spacer_regions.bed > "$output_file"
 
-# Clean up temporary files
-rm temp_output.txt spacers.bed
+num_spacers=$(wc -l < spacer_regions.bed)
+echo "Number of spacers extracted: $num_spacers"
+
+
+rm temp_output.txt perfect_matches.bed spacer_regions.bed
+
 
