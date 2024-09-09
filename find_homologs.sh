73af98067e24161466e85de2e3e19efa36fb29ee
ilahamusayeva93 <ilahamusayeva06@gmail.com>
2024-09-08 20:07:05 -0400

    Modify find_homologs.sh to perform BLAST search and filter results

--- find_homologs.sh
+++ find_homologs.sh
@@ -2,24 +2,30 @@
 
 
 if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <query file> <subject file> <output file>"
+    echo "Usage: $0 <protein query file> <nucleotide database> <output file>"
     exit 1
 fi
 
+
 query_file=$1
-subject_file=$2
+subject_db=$2
 output_file=$3
+
 
 query_length=$(grep -v ">" "$query_file" | tr -d '\n' | wc -c)
 
-blastn -query "$query_file" -subject "$subject_file" -task blastn-short \
--outfmt '6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore' > temp_output.txt
 
-awk -v len="$query_length" '$3 == 100 && $4 == len && $5 == 0 && $6 == 0' temp_output.txt > "$output_file"
+tblastn -query "$query_file" -db "$subject_db" -out temp_output.txt \
+-outfmt '6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore'
 
-perfect_matches_count=$(wc -l < "$output_file")
-echo "Number of perfect matches: $perfect_matches_count"
+
+awk -v qlen="$query_length" '$3 > 30 && $4 > 0.9*qlen' temp_output.txt > "$output_file"
+
+
+matches_count=$(wc -l < "$output_file")
+echo "Number of matches: $matches_count"
 
 
 rm temp_output.txt
+
 
