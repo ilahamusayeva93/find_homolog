c1a6bcb14c099d21ff7147635e651a342ac5ede3
Ilaha Musayeva <147144821+ilahamusayeva93@users.noreply.github.com>
2024-09-09 08:37:03 -0400

    find_homologs.sh

--- find_homologs.sh
+++ find_homologs.sh
@@ -2,30 +2,23 @@
 
 
 if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <protein query file> <nucleotide database> <output file>"
-    exit 1
+  echo "Usage: ./find_homologs.sh <query file> <subject file> <output file>"
+  exit 1
 fi
 
 
 query_file=$1
-subject_db=$2
+subject_file=$2
 output_file=$3
 
 
-query_length=$(grep -v ">" "$query_file" | tr -d '\n' | wc -c)
+tblastn -query "$query_file" -subject "$subject_file" -out blast_results.txt -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore"
 
 
-tblastn -query "$query_file" -db "$subject_db" -out temp_output.txt \
--outfmt '6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore'
+awk -v qlen="$qlen" '{if ($3 > 30 && $4 > (0.9 * qlen)) print $0}' blast_results.txt > "$output_file"
+
+num_matches=$(wc -l < "$output_file")
+echo "Number of matches found: $num_matches"
 
 
-awk -v qlen="$query_length" '$3 > 30 && $4 > 0.9*qlen' temp_output.txt > "$output_file"
-
-
-matches_count=$(wc -l < "$output_file")
-echo "Number of matches: $matches_count"
-
-
-rm temp_output.txt
-
-
+rm blast_results.txt
